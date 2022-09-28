a54073cfcfa71b387957a32b04d01a42ebf99491
Attapon Thanawong <attapon_srem@hotmail.com>
2022-09-28 16:06:49 +0700

    add template swagger

---
+++ tlp/fiberv2/_main-dev.go
@@ -0,0 +1,13 @@
+package main
+
+// @title       API
+// @version     1
+// @description API Service
+// @contact.name
+// @contact.url
+// @contact.email
+// @schemes                   http
+// @host                      localhost:8888
+// @BasePath                  /api
+// @securityDefinitions.basic BasicAuth
+func main() {}
---
+++ tlp/fiberv2/_main-prd.go
@@ -0,0 +1,11 @@
+package main
+
+// @title                     Example API
+// @version                   2
+// @description               API Service
+// @schemes                   https
+// @host                      example.com
+// @BasePath                  /
+// @securityDefinitions.basic BasicAuth
+func main() {
+}
